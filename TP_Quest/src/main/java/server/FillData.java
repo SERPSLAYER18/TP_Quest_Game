b6a6f2ec1320f36121abcee14277859e44708668
Kirill Lyahnovich <kirill.lyahnovich@gmail.com>
2021-04-04 14:46:01 +0300

    websocket test server

--- TP_Quest/src/main/java/server/FillData.java
+++ TP_Quest/src/main/java/server/FillData.java
@@ -1,8 +1,8 @@
 package server;
 
+
 import dto.QuestionDto;
 import dto.UserDto;
-
 import service.DBService;
 
 import java.sql.SQLException;
@@ -11,9 +11,6 @@ import java.util.List;
 import java.util.Random;
 
 public class FillData {
-
-    public static DBService service;
-
     public static void fillUsers() throws SQLException {
         List<UserDto> users = new ArrayList<>();
         users.add(new UserDto("Kirill", "1234--"));
@@ -21,45 +18,43 @@ public class FillData {
         users.add(new UserDto("Liza", "рейкик"));
         users.add(new UserDto("Grape", "да"));
 
+        DBService service = new DBService();
         service.cleanUpUsers();
-
         Random random = new Random();
-        for (var user : users) {
-            user.setScore(Math.abs(random.nextInt() % 100));
+        for (UserDto user : users) {
+            user.setScore(Math.abs(random.nextInt() % 1000));
             service.saveUser(user);
-        }
-        for (var user : users) {
-            service.updateUserRecord(user, Math.abs(random.nextInt() % 20));
         }
 
     }
 
-    public static void fillQuestionDtos() throws SQLException {
+    public static void fillQuestions() throws SQLException {
+        ArrayList<QuestionDto> questions = new ArrayList<>();
+        questions.add(new QuestionDto("ЕГО поднял над Скалой Прайда мандрил Рафики","Симба","Кино","100"));
+        questions.add(new QuestionDto("\"Кинопробы\" ОНИ записали песню Виктора Цоя \"Следи за собой\"","Король и шут","Кино","200"));
+        questions.add(new QuestionDto("ОН сыграл главную роль в фильме \"Тюремный рок\"","Элвис Пресли","Кино","300"));
+        questions.add(new QuestionDto("ЭТОТ ФИЛЬМ далек от литературного первоисточника. Например, Гудвин там становится убийцей","Короли и капуста","Кино","400"));
+        questions.add(new QuestionDto("В фильме 1926 года ОН становится командиром подразделения Рабоче-крестьянской Красной Армии","Беня Крик","Кино","500"));
+        questions.add(new QuestionDto("Крокодил Гена пел:\nПрилетит вдруг волшебник\nВ голубом вертолете,\nИ бесплатно покажет кино.\"\n Автор музыки к этой песне..","Владимир Шаинский","Кино","300"));
+        questions.add(new QuestionDto("Этот японский поэт Х в. составил первую антологию японской поэзии (достаточно фамилии)","Кино","Кино","200"));
+        questions.add(new QuestionDto("Так назывался первый в истории группы \"Кино\" альбом, записанный в профессиональной студии","Звезда по имени Солнце","Кино","100"));
+        questions.add(new QuestionDto("В пионерской песне с рефреном \"неправильно, неправильно кончается кино!\" осуждается его страшная гибель.","Чапаева ","Кино","400"));
+        questions.add(new QuestionDto(" Именно он был первым ведущим передачи \"Кинопанорама\"","Алексей Каплер.","Кино","500"));
+        questions.add(new QuestionDto(" ТАК называется биографический фильм о жизни рок-музыканта Иэна Дьюри","Секс, наркотики и рок-н-ролл","Кино","400"));
 
-        List<QuestionDto> questions = new ArrayList<>();
-        questions.add(new QuestionDto("ЕГО поднял над Скалой Прайда мандрил Рафики", "Симба", "Кино", "100"));
-        questions.add(new QuestionDto("\"Кинопробы\" ОНИ записали песню Виктора Цоя \"Следи за собой\"", "Король и шут", "Кино", "200"));
-        questions.add(new QuestionDto("ОН сыграл главную роль в фильме \"Тюремный рок\"", "Элвис Пресли", "Кино", "300"));
-        questions.add(new QuestionDto("ЭТОТ ФИЛЬМ далек от литературного первоисточника. Например, Гудвин там становится убийцей", "Короли и капуста", "Кино", "400"));
-        questions.add(new QuestionDto("В фильме 1926 года ОН становится командиром подразделения Рабоче-крестьянской Красной Армии", "Беня Крик", "Кино", "500"));
-        questions.add(new QuestionDto("Крокодил Гена пел:\nПрилетит вдруг волшебник\nВ голубом вертолете,\nИ бесплатно покажет кино.\"\n Автор музыки к этой песне..", "Владимир Шаинский", "Кино", "300"));
-        questions.add(new QuestionDto("Этот японский поэт Х в. составил первую антологию японской поэзии (достаточно фамилии)", "Кино", "Кино", "200"));
-        questions.add(new QuestionDto("Так назывался первый в истории группы \"Кино\" альбом, записанный в профессиональной студии", "Звезда по имени Солнце", "Кино", "100"));
-        questions.add(new QuestionDto("В пионерской песне с рефреном \"неправильно, неправильно кончается кино!\" осуждается его страшная гибель.", "Чапаева ", "Кино", "400"));
-        questions.add(new QuestionDto("Именно он был первым ведущим передачи \"Кинопанорама\"", "Алексей Каплер.", "Кино", "500"));
-        questions.add(new QuestionDto("ТАК называется биографический фильм о жизни рок-музыканта Иэна Дьюри", "Секс, наркотики и рок-н-ролл", "Кино", "400"));
+
+        DBService service = new DBService();
 
         service.clearQuestions();
-
-        for (var q : questions) {
+        for(var q:questions)
+        {
             service.saveQuestion(q);
         }
 
     }
 
     public static void main(String[] args) throws SQLException {
-        service = new DBService();
         fillUsers();
-        fillQuestionDtos();
+        fillQuestions();
     }
 }
--- TP_Quest/src/main/java/server/websocketTest/QuestionController.java
+++ TP_Quest/src/main/java/server/websocketTest/QuestionController.java
@@ -10,13 +10,12 @@ import dto.QuestionQueryDto;
 @Controller
 public class QuestionController {
 
-    @MessageMapping("/QuestionDto")
-    @SendTo("/topic/QuestionDtos")
-    public QuestionDto randomQuestionDto(QuestionQueryDto questionDtoQuery) throws Exception {
-        QuestionDto questionDto = DBService.instance.getRandomQuestion(
+    @MessageMapping("/question")
+    @SendTo("/topic/questions")
+    public QuestionDto randomQuestionDto(QuestionQueryDto questionDtoQuery) {
+        return DBService.instance.getRandomQuestion(
                 questionDtoQuery.getTopic(),
                 questionDtoQuery.getDifficulty());
-        return questionDto;
     }
 
 }
